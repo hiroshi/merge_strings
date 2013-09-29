8cd3c2ae78d054028d4e4ef9a13b5759b4d595b8
Hiroshi Saito <hiroshi3110@gmail.com>
2013-09-29 11:32:09 +0900

    Move the script to bin directory to be a rubygem

---
+++ bin/merge_strings.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+# extract_comments("/* abc */ /* 123 */")
+# => ["abc", "123"]
+def extract_comments(str)
+  (str || "").scan(%r"/\*\s*(.*?)\s*\*/").flatten
+end
+
+# key_value_line("foo", "bar", ["abc", "123"])
+# => '"foo" = "bar"; /* abc */ /* 123 */'
+def key_value_line(key, value, comments)
+  line = "\"#{key}\" = \"#{value}\";"
+  unless comments.empty?
+    line = ([line] + comments.map{|c|"/* #{c} */"}).join(" ")
+  end
+  line
+end
+
+if ARGV.count < 2
+  puts <<-USAGE
+Usage: merge_strings BASE.strings OTHER.strings [default:"TRANSLATION REQUIRED"]
+
+See https://github.com/hiroshi/merge_strings for more information.
+    USAGE
+  exit 2
+end
+base_strings_path = ARGV[0]
+other_strings_path = ARGV[1]
+translation_required = ARGV[2] || "TRANSLATION REQUIRED"
+# Extract values of other strings
+other_strings = {} #=> {"key": {val: "...", comments: ["...", "..."]}, ...}
+File.read(other_strings_path).each_line do |line|
+  if match = line.match(/"([^"]*)"\s*=\s*"([^"]*)"\s*;(.*)$/)
+    key, val, comments = $1, $2, extract_comments($3)
+    # puts "#{key} = #{val}"
+    other_strings[key] = {val: val, comments: comments}
+  end
+end
+# Copy base strings file then replace values with other_strings
+File.open(other_strings_path, "w+") do |other_strings_file|
+  File.read(base_strings_path).each_line do |line|
+    line.sub!(/"([^"]*)"\s*=\s*"([^"]*)"\s*;(.*)$/) do |m|
+      key, val, comments = $1, $2, extract_comments($3)
+      #puts "#{key} : #{comments}"
+      if other = other_strings[key]
+        key_value_line(key, other[:val], comments | other[:comments])
+      else
+        key_value_line(key, key, comments | [translation_required])
+      end
+    end
+    other_strings_file.puts line
+  end
+end
---
+++ merge_strings.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'merge_strings'
+  s.version     = '0.0.1'
+  s.executables << "merge_strings.rb"
+  s.date        = '2013-09-29'
+  s.summary     = "merge_strings.rb en.lproj/Localizable.strings ja.lproj/Localizable.strings"
+  s.description = "A scirpt to help maintaining Localizable.strings."
+  s.authors     = ["Hiroshi Saito"]
+  s.email       = 'hiroshi3110@gmail.com'
+  s.homepage    = 'https://github.com/hiroshi/merge_strings'
+  s.license       = 'MIT'
+end
